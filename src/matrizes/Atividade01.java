5124606bef9197bb185e1bf4af1975032303ea66
Anderson Martins <anderson.aguiar@outlook.com.br>
2025-08-12 19:33:13 -0300

    Add enunciado da atividade 1

--- src/matrizes/Atividade01.java
+++ src/matrizes/Atividade01.java
@@ -4,6 +4,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Atividade01 {
+    /*1. Análise de Saldos em Contas Correntes
+Um banco monitora os saldos diários de N clientes durante M dias em uma matriz double[N][M].
+Crie um programa que permita:
+Métodos a implementar:
+double saldoFinal(double[][] saldos, int cliente) – retorna o saldo do cliente ao final do período.
+double[] saldoFinalTodos(double[][] saldos) – retorna o saldo final de todos os clientes.
+void imprimirExtratoClientes(double[][] saldos) – exibe todos os saldos por cliente em formato tabular.
+*/
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double[][] saldosClientes = {
